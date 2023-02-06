29f02ab119bdb4b2451b19f39bae4e8c5b2bb9e1
yojulab <otter.oh@gmail.com>
2023-02-06 12:52:04 +0900

    add using api

---
+++ src/ApiExplorersWithData_go_kr.java
@@ -0,0 +1,50 @@
+// refer source : https://fbtmdwhd33.tistory.com/264
+// api : 외교부_국가·지역별 최신 안전소식(코로나 관련) 데이터(from https://www.data.go.kr/)
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ApiExplorersWithData_go_kr {
+    public static void main(String[] args) throws IOException {
+    	// 1. URL을 만들기 위한 StringBuilder.
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1262000/CountryCovid19SafetyServiceNew/getCountrySafetyNewsListNew"); /*URL*/
+        // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=BoygPZjC27pxm92hSposjnSob2u36vziS1rzIzxkrL9QxmlhB0SMARwLfNlBE3wrE7nnw34zLmmv0a6amvW4xg=="); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*XML 또는 JSON*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("cond[country_nm::EQ]","UTF-8") + "=" + URLEncoder.encode("가나", "UTF-8")); /*한글 국가명*/
+        urlBuilder.append("&" + URLEncoder.encode("cond[country_iso_alp2::EQ]","UTF-8") + "=" + URLEncoder.encode("GH", "UTF-8")); /*ISO 2자리코드*/
+        // 3. URL 객체 생성.
+        URL url = new URL(urlBuilder.toString());
+        // 4. 요청하고자 하는 URL과 통신하기 위한 Connection 객체 생성.
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        // 5. 통신을 위한 메소드 SET.
+        conn.setRequestMethod("GET");
+        // 6. 통신을 위한 Content-type SET. 
+        conn.setRequestProperty("Content-type", "application/json");
+        // 7. 통신 응답 코드 확인.
+        System.out.println("Response code: " + conn.getResponseCode());
+        // 8. 전달받은 데이터를 BufferedReader 객체로 저장.
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        // 9. 저장된 데이터를 라인별로 읽어 StringBuilder 객체로 저장.
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        // 10. 객체 해제.
+        rd.close();
+        conn.disconnect();
+        // 11. 전달받은 데이터 확인.
+        System.out.println(sb.toString());
+    }
+}
